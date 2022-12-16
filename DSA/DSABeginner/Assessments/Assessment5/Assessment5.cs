e707e2a64adcbc34a6a3b8c03a0ca61f8425a913
sskumargan <ganku007@live.in>
2022-12-17 03:31:50 +0530

    dsa

--- DSA/DSABeginner/Assessments/Assessment5/Assessment5.cs
+++ DSA/DSABeginner/Assessments/Assessment5/Assessment5.cs
@@ -776,7 +776,7 @@ namespace DSABeginner.Assessments.Assessment5
                 {
                     if (nums[i] + nums[l] + nums[r] < target)
                     {
-                        count++; //count += r - l;
+                        count += r - l;
                         l++;
                     }
                     else
@@ -784,6 +784,50 @@ namespace DSABeginner.Assessments.Assessment5
                 }
             }
             return count;
+        }
+        /*
+         * https://leetcode.com/problems/3sum-closest
+         
+           Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+           Return the sum of the three integers.
+           
+           You may assume that each input would have exactly one solution.
+           
+           Example 1:
+           
+           Input: nums = [-1,2,1,-4], target = 1
+           Output: 2
+           Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+           Example 2:
+           
+           Input: nums = [0,0,0], target = 1
+           Output: 0
+           Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+        */
+
+        public static int Find3SumCloser(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int actSum = int.MaxValue;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int l = i + 1;
+                int r = nums.Length - 1;
+                while (l < r)
+                {
+                    int sum = nums[i] + nums[l] + nums[r];
+                    if (Math.Abs(sum - target) < Math.Abs(actSum - target))
+                    {
+                        actSum = sum;
+                    }
+                    if (sum > target)
+                        r--;
+                    else
+                        l++;
+                }
+            }
+            return actSum;
         }
     }
 
