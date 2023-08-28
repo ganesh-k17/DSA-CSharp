48dd771e9203bf7a3a7307c366a62ffcd4481c58
sskumargan <ganku007@live.in>
2023-08-28 22:17:10 +0530

    45 must sql queries

---
+++ DSA/DSABeginner/30Must/30Must.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSABeginner._30Must
+{
+    internal class _30Must
+    {
+        public static string AdditionOfBinaryString(string a, string b)
+        {
+            string result = string.Empty;
+            int aLen = a.Length;
+            int bLen = b.Length;
+            int i = aLen > bLen ? aLen-1 : bLen-1;
+            while (i < 0)
+            {
+
+            }
+            return result;
+        }
+    }
+}
---
+++ DSA/DSABeginner/45SQLMust/45Must.sql
@@ -0,0 +1,73 @@
+﻿/* 1. 
+Write an SQL query to report the first name, last name, city, and state of each person in the Person
+table. If the address of a personId is not present in the Address table, report null instead.
+
+Person(personId, firstName, lastName), Address(addressId, personId, city, state)
+*/
+select p.personId, p.firstName, p.lastName, a.city, a.state
+from person p
+left join address a on p.personId = a.personId; 
+
+/* 2 & 3.
+Second Highest Salary
+tag: Nth Highest Salary
+Write an SQL query to report the second highest salary from the Employee table. If there is no second
+highest salary, the query should report null.
+
+employee (id, salary)
+*/
+
+SELECT DISTINCT salary
+FROM employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+
+
+SELECT DISTINCT salary
+FROM (
+  SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank
+  FROM employee
+) ranked
+WHERE rank = 2;
+
+
+SELECT MAX(salary) 
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee);
+
+/* 4
+Write an SQL query to rank the scores, The ranking should e calculated according to the
+2ollowing rules1
+ The scores should be ranked 2rom the highest to the lowest+
+ If there is a tie between two scores, both should have the same ranking+
+ After a tie, the next ranking number should be the next consecutive integer value, In other
+words, there should be no holes between ranks,
+id is the primary key 2or this table.
+
+Each row o2 this tale contains the score o2 a game,
+Score is a floating point value with two decimal
+places,
+Return the result table ordered by score in descending order.
+
+Table => Scores (int score)
+*/
+
+
+
+select score, dense_rank() over (order by score desc) as rank from scores
+
+
+/* 5.
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Table=> Logs( id int, num varchar)
+*/
+
+SELECT num
+FROM (
+    SELECT num, 
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num, 
+           LAG(num, 1) OVER (ORDER BY id) AS prev_num
+    FROM Logs
+) t
+WHERE num = next_num AND num = prev_num;
